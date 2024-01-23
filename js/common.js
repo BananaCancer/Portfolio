28fe498afd6c9f9094af3a9526a3b0b8f3d3fbd5
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2024-01-23 13:43:22 -0500

    responsive nav

--- js/common.js
+++ js/common.js
@@ -1,16 +1,24 @@
+function increaseContactHeight() {
+  $('.square:nth-child(1)').css('height', '60px');
+  $('.square:nth-child(2)').css('height', '60px');
+  $('.square:nth-child(3)').css('height', '60px');
+  $('.square:nth-child(4)').css('height', '60px');
+}
+function reduceContactHeight() {
+  $('.square:nth-child(1)').css('height', '0px');
+  $('.square:nth-child(2)').css('height', '0px');
+  $('.square:nth-child(3)').css('height', '0px');
+  $('.square:nth-child(4)').css('height', '0px');
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    $('.dropdown').hover(
-        function () {
-            $('.square:nth-child(1)').css('height', '60px');
-            $('.square:nth-child(2)').css('height', '60px');
-            $('.square:nth-child(3)').css('height', '60px');
-            $('.square:nth-child(4)').css('height', '60px');
-          },
-          function () {
-            $('.square:nth-child(1)').css('height', '0px');
-            $('.square:nth-child(2)').css('height', '0px');
-            $('.square:nth-child(3)').css('height', '0px');
-            $('.square:nth-child(4)').css('height', '0px');
-          }
-      );
+  
+  $('.dropdown').hover(increaseContactHeight, reduceContactHeight)
+  $('.dropdown').click(function() {
+    if ($('.square:nth-child(1)').css('height') == "0px") {
+      increaseContactHeight()
+    } else {
+      reduceContactHeight()
+    }
+  })
 });
